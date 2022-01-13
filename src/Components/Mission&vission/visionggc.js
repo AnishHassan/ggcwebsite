85d6c3233dd687af0a383cc79cd93c6fe62d102b
Anish Hassan <anish.hassanworks@gmail.com>
2022-01-14 01:28:55 +0500

    updatedss

--- src/Components/Mission&vission/visionggc.js
+++ src/Components/Mission&vission/visionggc.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Tilt from 'react-tilt'
 import mission from '../../images/Mission.png'
 import vision from '../../images/Vision.png'
-import {MVisionContainer,MVisionRow,MVisionColumnS,MVisionImg } from './VisionggcElements'
+import {MVisionContainer,MVisionRow,MVisionColumnS,MVisionImg } from './visionggcElements'
 function Visionggc() {
     return (
         <>
