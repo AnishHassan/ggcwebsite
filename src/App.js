cfde6a737e0f1a9bdd60304efba04b9cf3f69424
Anish Hassan <anish.hassanworks@gmail.com>
2022-01-14 01:30:05 +0500

    updatedsss

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
 import Sidebar from './Components/Sidebar/index.js'
 import Navbar from './Components/Navbar/index.js';
 import Hero from './Hero'
-import Visionggc from './Components/Mission&vission/visionggc.js'
+import Visionggc from './Components/Mission&vission/Visionggc.js'
 import ContactUs from './Components/ContactUs/ContactUs.js';
 import FooterBar from './Components/Footer/index'
 function App() {
